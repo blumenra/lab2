b4c14b720db4512fa8710b5ab08b161a08f05f57
Alon Blumenreich <alon@mail.com>
2017-03-27 22:27:30 +0300

    Finished T2a

--- task2/task2File.c
+++ task2/task2File.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
  
 char censor(char c) {
@@ -10,10 +11,20 @@ char censor(char c) {
  
 char* map(char *array, int array_length, char (*f) (char)){
   char* mapped_array = (char*)(malloc(array_length*sizeof(char)));
-  /* TODO: Complete during task 2.a */
+  
+  int i;
+
+  for(i=0; i<array_length; i++){
+
+  	mapped_array[i] = f(array[i]);
+  }
+
   return mapped_array;
 }
  
 int main(int argc, char **argv){
-  /* TODO: Test your code */
+  	char arr1[] = {'H','E','Y','!', '!', 'A', 0};
+	char* arr2 = map(arr1, 7, censor);
+	printf("%s\n", arr2); /* HEY. */
+	free(arr2);
 }
