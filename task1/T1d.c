0acd8d80361315dd7ad9fa8e48be124c53d10a30
Alon Blumenreich <alon@mail.com>
2017-03-27 21:58:40 +0300

    FInished T1d

---
+++ task1/T1d.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char** argv){
+
+	int i;
+
+	int iarray[] = {1,2,3};
+	char carray[] = {'a','b','c'};
+	int* iarrayPtr;
+	char* carrayPtr;
+
+	iarrayPtr = iarray;
+	carrayPtr = carray;
+
+
+	for(i=0; i<3; i++){
+		printf("%d ", *(iarrayPtr + i));
+	}
+
+	puts("");
+
+	for(i=0; i<3; i++){
+		printf("%c ", *(carrayPtr + i));
+	}
+
+	puts("");
+	
+	return 0;
+}
